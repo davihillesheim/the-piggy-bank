e8ce20538c7eafbac3f3914317bc346dc0388003
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-03-06 18:38:09 +0000

    update section contents

--- src/components/Section/Contents.js
+++ src/components/Section/Contents.js
@@ -5,38 +5,38 @@ import mobile from '../../images/mobile.svg';
 export const firstSection = {
   id: 'about',
   lightBackground: false,
-  headline: 'Some headline',
-  title: 'bla bla bla big text bla bla bla big text bla bla bla big text',
-  subtitle: 'bla bla bla bla small text explaining stuff bla bla bla bla small text explaining stuff',
+  headline: 'Track your expenses',
+  title: 'You can track all your expenses by category',
+  subtitle: 'Keep track of your expenses by registering them by category, with the possibility of filtering them to pinpoint where you can save more.',
   button: 'Get started',
   imgFirst: false,
   img: detailed_expenses,
-  alt: 'Img description',
+  alt: 'Detailed Expenses',
   primary: true
 }
 
 export const secondtSection = {
   id: 'about',
   lightBackground: true,
-  headline: 'Some headline',
-  title: 'bla bla bla big text bla bla bla big text bla bla bla big text',
-  subtitle: 'bla bla bla bla small text explaining stuff bla bla bla bla small text explaining stuff',
-  button: 'Get started',
+  headline: 'Comprehensive graphs',
+  title: 'See your expenses more clearly through our graphs',
+  subtitle: 'You can visualize all your expenses throughou the months and organized by category too.',
+  button: '?????',
   imgFirst: true,
   img: graphs,
-  alt: 'Img description',
+  alt: 'Graph',
   primary: true
 }
 
 export const thirdSection = {
   id: 'about',
   lightBackground: false,
-  headline: 'Some headline',
-  title: 'bla bla bla big text bla bla bla big text bla bla bla big text',
-  subtitle: 'bla bla bla bla small text explaining stuff bla bla bla bla small text explaining stuff',
-  button: 'Get started',
+  headline: 'Everything responsive',
+  title: 'Register your expenses on the go',
+  subtitle: 'Our website is fully responsive, so you can access everything through your phone and register everything on the go.',
+  button: '??????',
   imgFirst: false,
   img: mobile,
-  alt: 'Img description',
+  alt: 'mobile',
   primary: true
 }

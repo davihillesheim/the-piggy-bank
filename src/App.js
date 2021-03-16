f027d48ac40e59f6b455d4145f193b33e855f8d4
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-03-16 11:55:53 +0000

    update App.js to the new modifications in the components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/pages';
 import SigninPage from './components/pages/SigninPage';
+import ContactPage from './components/pages/ContactPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={SigninPage} />
+        <Route exact path="/contact" component={ContactPage} />
       </Switch>
     </Router>
   );

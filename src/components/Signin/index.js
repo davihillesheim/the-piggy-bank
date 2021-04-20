bacbe4e75bd022013f9c1d0670c7d07389feb754
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-04-20 11:20:25 -0300

    basic signin request to the backend with matching check

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,8 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import "./Signin.css";
 
 const Signin = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const history = useHistory();
+
+  const onChangeEmail = event => {
+    setEmail(event.target.value);
+  }
+
+  const onChangePassword = event => {
+    setPassword(event.target.value);
+  }
+
+  const onSubmitSignin = () => {
+    fetch('http://localhost:3001/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data === 'success') {
+        history.push("/");
+      } else {
+        console.log('didn\'t work');
+      }
+    })
+  }
+
   return (
     <div className="signin-container">
       <div className="form-container">
@@ -17,15 +49,15 @@ const Signin = () => {
             <label className="form-label" htmlFor="for">
               Email
             </label>
-            <input className="form-input" type="email" required />
+            <input className="form-input" type="email" required value={email} onChange={onChangeEmail}/>
             <label className="form-label" htmlFor="for">
               Password
             </label>
-            <input className="form-input" type="password" required />
-            <button className="form-button" type="submit">
+            <input className="form-input" type="password" required value={password} onChange={onChangePassword}/>
+            <button className="form-button" type="button" onClick={onSubmitSignin}>
               Login
             </button>
-            <span className="form-text">Forgot password</span>
+            <span className="form-text">Register</span>
           </form>
         </div>
       </div>

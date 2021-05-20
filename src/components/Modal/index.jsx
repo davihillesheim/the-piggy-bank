17d2d966bba6cd2f9c65abd3f32e0105c5200d55
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-05-20 15:26:45 -0300

    add expense creation throgh the frontend

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { useEffect, useState } from 'react/cjs/react.development';
 import './Modal.css';
 
 const Modal = ({ id = 'modal', onClose = () => { }, categories }) => {
-  const handleOutsideClick = (e) => {
-    if (e.target.id === id) onClose();
+
+  const userId = Number(localStorage.getItem('loggedUser'));
+  const [categoryId, setCategoryId] = useState();
+  const [amount, setAmount] = useState();
+  const [description, setDescription] = useState('');
+  const date = '2021-05-20';
+
+  useEffect(() => {
+    console.log(typeof(userId))
+    console.log(typeof(categoryId))
+    console.log(typeof(amount))
+    console.log(typeof(description))
+    console.log(typeof(date))    
+  },[])
+
+  const handleOutsideClick = event => {
+    if (event.target.id === id) onClose();
+  }
+
+  const onChangeCategoryId = value => {
+    setCategoryId(Number(value));
+  }
+
+  const onChangeAmount = event => {
+    setAmount(Number(event.target.value));
+  }
+
+  const onChangeDescription = event => {
+    setDescription(event.target.value);
+  }
+
+  const onSubmitExpense = (userId, categoryId, amount, description, date) => {
+    fetch('http://localhost:3001/expenses', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        user_id: userId,
+        category_id: categoryId,
+        amount: amount,
+        description: description,
+        date: date
+      })
+    }).then(response => response.json()).then(expense => {
+      if(expense.user_id) {
+        console.log('Expense create successfully');
+      } else {
+        console.log('Failed to create expense');
+      }
+    })
   }
 
   return (
@@ -11,14 +59,21 @@ const Modal = ({ id = 'modal', onClose = () => { }, categories }) => {
       <div className='modal-container'>
         <button className='close' onClick={onClose}>close</button>
         <ul className='category-grid'>
-          {categories.map(category => <li key={category.id}>
+          {categories.map(category => <li key={category.id} onClick={() => onChangeCategoryId(category.id)}>
             <img src={category.icon_url} alt={category.name} />
             <span>
               {category.name}
             </span>
           </li>)}
         </ul>
+        <div className='modal-input'>
+          <label htmlFor="for">Amount</label>
+          <input type="number" step="any" required value={amount} onChange={onChangeAmount}/>
+          <label htmlFor="for">Description(optional)</label>
+          <input type="text" required value={description} onChange={onChangeDescription}/>
+        </div>
         <div className='content'></div>
+        <button className='submit-expense' onClick={() => onSubmitExpense(userId, categoryId, amount, description, date)}>Ok</button>
       </div>
     </div>
   )
